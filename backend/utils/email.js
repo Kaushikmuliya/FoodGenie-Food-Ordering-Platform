e22d7fde1a53d62df77cdbc4c6da2ad149a743de
Kaushikmuliya <kaushikmuliya82@gmail.com>
2026-07-28 20:18:42 +0530

    feat: add error handling middleware and utility modules

---
+++ backend/utils/email.js
@@ -0,0 +1,55 @@
+const nodemailer = require("nodemailer");
+const pug = require("pug");
+const htmlToText = require("html-to-text");
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `OrderIt <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(`${__dirname}/../view/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.convert(html),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "welcome to the Order It!");
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "password reset token (valid for only 10 minutes)"
+    );
+  }
+};
+
+
