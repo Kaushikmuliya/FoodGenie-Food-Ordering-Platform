5bff0b5f2b79f14954abcc2476a442ee33fad83b
Kaushikmuliya <kaushikmuliya82@gmail.com>
2026-07-28 20:36:50 +0530

    chore: rename project from OrderIt to FoodGenie

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -7,7 +7,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(" ")[0];
     this.url = url;
-    this.from = `OrderIt <${process.env.EMAIL_FROM}>`;
+    this.from = `FoodGenie <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
@@ -41,7 +41,7 @@ module.exports = class Email {
   }
 
   async sendWelcome() {
-    await this.send("welcome", "welcome to the Order It!");
+    await this.send("welcome", "welcome to FoodGenie!");
   }
 
   async sendPasswordReset() {
